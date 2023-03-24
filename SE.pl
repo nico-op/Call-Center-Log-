6f410d358aa82699a6b6e14f3307a097c893c230
Gabriel Lobo <kevinlobo1920k@gmail.com>
2023-03-24 01:54:39 -0600

    permite brindar una solución fase-inicial

--- SE.pl
+++ SE.pl
@@ -1,13 +1,8 @@
 %Sistema Experto.
-
 palabra_buscar('wifi').
 palabra_buscar('modem').
 palabra_buscar('drivers').
 palabra_buscar('puerto').
-
-
-
-
 es_la_palabra(Palabraclave):-palabra_buscar(Palabraclave).
 
 %Recorre una oración.
@@ -19,3 +14,59 @@ lista_palabras_clave([Palabra|Resto],[Palabra|Palabra_clave_encontrada]):-
 
 lista_palabras_clave([_|Resto],Palabra_clave_encontrada):-
     lista_palabras_clave(Resto,Palabra_clave_encontrada).
+
+
+
+% Primer enfoque generar solución a partir de las palabras claves que se
+% vayan creando
+%
+
+
+solucion():-
+    write('Brinde la información que se le pide: '),nl,
+    read_line_to_codes(Respuestausuario,, Codes),
+    atom_codes(Respuestausuario,, Codes),
+    (   sub_atom(Respuestausuario, _,_,_,'si')
+    ->  R = 'Sí'
+    ; sub_atom(Respuestausuario, _,_,_,'no')
+    ->  R = 'No'
+    ; (sub_atom(Respuestausuario, _,_,_,'wifi')
+    ;    sub_atom(Respuestausuario, _,_,_,'drivers')
+    ;    sub_atom(Respuestausuario, _,_,_,'cable')
+      )
+    ->  R = 'Es posible que presente problemas con: '
+    ;   R = 'Está respuesta no es valida, estamos trabajando para que ampliar la categoría de conocimiento'
+    ),
+    write(R),nl.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
